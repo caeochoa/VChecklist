997ba7f150310c6d0e8d28955d0d4daf24d14f1e
caeochoa <caeochoa@gmail.com>
2022-07-06 21:15:15 +0100

    added config file to vchecklist.sh

--- vchecklist.sh
+++ vchecklist.sh
@@ -8,6 +8,7 @@ mkdir /disk/scratch/s2259310 /disk/scratch/s2259310/nnUNet_raw_data_base /disk/s
 echo Copying data...
 #zip -r data.zip nnUNet_raw_data_base/nnUNet_raw_data/Task500_BraTS2021
 rsync -u /home/s2259310/VChecklist/nn-UNet/nnUNet_raw_data_base/nnUNet_raw_data/Task500_BraTS2021/data/data.zip /disk/scratch/s2259310
+rsync -u /home/s2259310/VChecklist/ImageMods/configs/test.csv /disk/scratch/s2259310/test_config.csv
 echo Done!
 echo Exporting data...
 unzip -u /disk/scratch/s2259310/data.zip -d /disk/scratch/s2259310/nnUNet_raw_data_base/nnUNet_raw_data/Task500_BraTS2021
@@ -19,7 +20,7 @@ echo Done!
 
 echo Running vc.py
 export nnUNet_raw_data_base="/disk/scratch/s2259310/nnUNet_raw_data_base"
-python vc.py -i /disk/scratch/s2259310/nnUNet_raw_data_base/nnUNet_raw_data/Task500_BraTS2021/images -o /disk/scratch/s2259310/outputs/Task500_BraTS2021
+python vc.py -i /disk/scratch/s2259310/nnUNet_raw_data_base/nnUNet_raw_data/Task500_BraTS2021/images -o /disk/scratch/s2259310/outputs/Task500_BraTS2021 -c /disk/scratch/s2259310/test_config.csv
 echo Done!!!! 
 
 echo Compressing outputs and copying them to home directory
