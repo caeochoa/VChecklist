83b211398710283fddc1e872ff12f7132fa9af9b
caeochoa <caeochoa@gmail.com>
2022-07-08 17:54:04 +0100

    added option to limit dataset to tests_full_data.sh

--- tests_full_data.sh
+++ tests_full_data.sh
@@ -11,7 +11,7 @@ rsync -u nn-UNet/nnUNet_raw_data_base/nnUNet_raw_data/Task500_BraTS2021/data/dat
 for i in (seq -f "%05g" $1 $2)
 do
     unzip -u /disk/scratch/s2259310/data.zip RSNA_ASNR_MICCAI_BraTS2021_TrainingData_16July2021/BraTS2021_${i} -d /disk/scratch/s2259310/nnUNet_raw_data_base/nnUNet_raw_data/Task500_BraTS2021
-
+done
 echo Done!
 
 echo Compressing and copying trained models
