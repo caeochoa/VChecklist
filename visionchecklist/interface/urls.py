05cfe27d3ece99159bbed1e8d4924e32075a7b1b
--global <caeochoa@gmail.com>
2022-07-26 02:12:42 +0200

    made index url better

--- visionchecklist/interface/urls.py
+++ visionchecklist/interface/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 app_name = "interface"
 urlpatterns = [
-    path('<int:pk>', views.HomeView.as_view(), name='image'),
+    path('', views.HomeView.as_view(), name='image',kwargs={"pk":1}),
     path('apply', views.select, name='select'),
     path('<str:mode>/<int:prop>/<int:angle>', views.ResultsView, name='results')
 ]
